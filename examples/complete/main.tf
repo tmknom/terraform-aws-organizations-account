f2af57112b3b4eb3fadcc4574853c29888349c54
tmknom <tmk.nom@gmail.com>
2018-11-28 00:32:34 +0900

    Implement complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,1 +1,9 @@
+module "organizations_account" {
+  source = "../../"
+  name   = "example"
+  email  = "complete@example.com"
 
+  iam_user_access_to_billing = "DENY"
+  role_name                  = "OrganizationAccoLuntAccessRole"
+  enabled                    = true
+}
